fb4205888bfc48a471e4ff6acc170535770dbace
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:43:44 +0330

    Add Workspace Model

---
+++ Domain/KanbanBoard.Domain/Models/Workspace.cs
@@ -0,0 +1,15 @@
+﻿namespace KanbanBoard.Domain.Models;
+
+public class Workspace
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public Guid OwnerId { get; set; }
+
+    public User Owner { get; set; } = null!;
+
+    //public ICollection<Board> Boards { get; set; }
+    //    = new List<Board>();
+}
