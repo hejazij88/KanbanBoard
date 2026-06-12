7c75e079f3d59b5e2b415b545a9a4f97340fde76
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:44:26 +0330

    Add Board Model

---
+++ Domain/KanbanBoard.Domain/Models/Board.cs
@@ -0,0 +1,15 @@
+﻿namespace KanbanBoard.Domain.Models;
+
+public class Board
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public Guid WorkspaceId { get; set; }
+
+    public Workspace Workspace { get; set; } = null!;
+
+    //public ICollection<BoardColumn> Columns { get; set; }
+    //    = new List<BoardColumn>();
+}
