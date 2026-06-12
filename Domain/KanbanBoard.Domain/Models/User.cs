68a2045ef51e50a7b0e345e28dc9554b1ef5e67a
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:41:45 +0330

    Add User Model

---
+++ Domain/KanbanBoard.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace KanbanBoard.Domain.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = null!;
+    public string HashPassword { get; set; } = null!;
+    public string Email { get; set; } = null!;
+
+}
