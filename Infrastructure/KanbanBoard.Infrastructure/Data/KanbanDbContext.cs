4f5a51bb1eda14c50f76f275d6162be813a91be3
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:44:18 +0330

    Add KanbanDbContext

---
+++ Infrastructure/KanbanBoard.Infrastructure/Data/KanbanDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KanbanBoard.Infrastructure.Data;
+
+public class KanbanDbContext:DbContext
+{
+
+    public KanbanDbContext(
+        DbContextOptions<KanbanDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Workspace> Workspaces => Set<Workspace>();
+
+    public DbSet<WorkspaceMember> WorkspaceMembers => Set<WorkspaceMember>();
+
+    public DbSet<Board> Boards => Set<Board>();
+
+    public DbSet<BoardColumn> Columns => Set<BoardColumn>();
+
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(KanbanDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
