a31a27d99d104a9961eda9a7e9ff47157da85678
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:52:15 +0330

    Add BoardConfiguration

---
+++ Infrastructure/KanbanBoard.Infrastructure/Configurations/BoardConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KanbanBoard.Infrastructure.Configurations;
+
+public class BoardConfiguration
+    : IEntityTypeConfiguration<Board>
+{
+    public void Configure(
+        EntityTypeBuilder<Board> builder)
+    {
+        builder.ToTable("Boards");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.HasOne(x => x.Workspace)
+            .WithMany(x => x.Boards)
+            .HasForeignKey(x => x.WorkspaceId);
+    }
+}
