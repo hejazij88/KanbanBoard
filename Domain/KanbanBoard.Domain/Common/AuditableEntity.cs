d31e57168e245f25e0d3b323b8fd2ed69b744155
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:54:02 +0330

    Add AuditableEntity

---
+++ Domain/KanbanBoard.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace KanbanBoard.Domain.Common;
+
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedAt { get; private set; }
+
+    public DateTime? UpdatedAt { get; private set; }
+
+    protected AuditableEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Update()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
