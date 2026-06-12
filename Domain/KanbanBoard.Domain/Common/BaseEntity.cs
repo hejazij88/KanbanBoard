e6f4f0fa10b6a141d9c889f2d940347f591bbcd5
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:53:18 +0330

    Add BaseEntity

---
+++ Domain/KanbanBoard.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace KanbanBoard.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+    }
+}
