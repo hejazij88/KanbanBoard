ba8b484237ac378aa9ab484305224a377e3974a6
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:51:11 +0330

    Add WorkspaceConfiguration

---
+++ Infrastructure/KanbanBoard.Infrastructure/Configurations/WorkspaceConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KanbanBoard.Infrastructure.Configurations;
+
+public class WorkspaceConfiguration
+    : IEntityTypeConfiguration<Workspace>
+{
+    public void Configure(
+        EntityTypeBuilder<Workspace> builder)
+    {
+        builder.ToTable("Workspaces");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
+}
