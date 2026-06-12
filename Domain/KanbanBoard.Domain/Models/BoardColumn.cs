1f35ac5d7cd0fc229c0eb7faceb733d74a3fd284
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:44:59 +0330

    Add BoardColumn

---
+++ Domain/KanbanBoard.Domain/Models/BoardColumn.cs
@@ -0,0 +1,17 @@
+﻿namespace KanbanBoard.Domain.Models;
+
+public class BoardColumn
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Order { get; set; }
+
+    public Guid BoardId { get; set; }
+
+    public Board Board { get; set; } = null!;
+
+    //public ICollection<TaskItem> Tasks { get; set; }
+    //    = new List<TaskItem>();
+}
