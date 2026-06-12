7a9b7a00cb98906077130de7783991fcae2cd03f
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:53:05 +0330

    Add TaskItemConfiguration

---
+++ Infrastructure/KanbanBoard.Infrastructure/Configurations/TaskItemConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KanbanBoard.Infrastructure.Configurations;
+
+public class TaskItemConfiguration
+    : IEntityTypeConfiguration<TaskItem>
+{
+    public void Configure(
+        EntityTypeBuilder<TaskItem> builder)
+    {
+        builder.ToTable("Tasks");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(4000);
+
+        builder.Property(x => x.Priority)
+            .HasConversion<int>();
+
+        builder.HasOne(x => x.Column)
+            .WithMany(x => x.Tasks)
+            .HasForeignKey(x => x.ColumnId);
+
+        builder.HasOne(x => x.AssignedUser)
+            .WithMany(x => x.AssignedTasks)
+            .HasForeignKey(x => x.AssignedUserId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
