eb4df4a811a29291153d870a1891f8483ed6bd1d
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 16:45:28 +0330

    Add TaskItem Model

---
+++ Domain/KanbanBoard.Domain/Models/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿namespace KanbanBoard.Domain.Models;
+
+public class TaskItem
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateTime? DueDate { get; set; }
+
+    public Priority Priority { get; set; }
+
+    public Guid ColumnId { get; set; }
+
+    public BoardColumn Column { get; set; } = null!;
+
+    public Guid? AssignedUserId { get; set; }
+
+    public User? AssignedUser { get; set; }
+}
