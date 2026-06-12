88ca3398ed92c76816ccc8b0fe4988feb15f16b5
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:51:54 +0330

    Add WorkspaceMemberConfiguration

---
+++ Infrastructure/KanbanBoard.Infrastructure/Configurations/WorkspaceMemberConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KanbanBoard.Infrastructure.Configurations;
+
+public class WorkspaceMemberConfiguration
+    : IEntityTypeConfiguration<WorkspaceMember>
+{
+    public void Configure(
+        EntityTypeBuilder<WorkspaceMember> builder)
+    {
+        builder.ToTable("WorkspaceMembers");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Workspaces)
+            .HasForeignKey(x => x.UserId);
+
+        builder.HasOne(x => x.Workspace)
+            .WithMany(x => x.Members)
+            .HasForeignKey(x => x.WorkspaceId);
+
+        builder.HasIndex(x =>
+                new
+                {
+                    x.UserId,
+                    x.WorkspaceId
+                })
+            .IsUnique();
+    }
+}
