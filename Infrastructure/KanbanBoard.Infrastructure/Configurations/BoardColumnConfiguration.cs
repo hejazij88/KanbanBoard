b3facea684d10d62bd660d9777e56d80a78fb9ef
98913 <98913@LAPTOP-JQR66TAF>
2026-06-12 17:52:41 +0330

    Add BoardColumnConfiguration

---
+++ Infrastructure/KanbanBoard.Infrastructure/Configurations/BoardColumnConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KanbanBoard.Infrastructure.Configurations;
+
+public class BoardColumnConfiguration
+    : IEntityTypeConfiguration<BoardColumn>
+{
+    public void Configure(
+        EntityTypeBuilder<BoardColumn> builder)
+    {
+        builder.ToTable("Columns");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.HasOne(x => x.Board)
+            .WithMany(x => x.Columns)
+            .HasForeignKey(x => x.BoardId);
+    }
+}
