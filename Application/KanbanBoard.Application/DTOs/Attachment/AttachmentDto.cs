cb1776aec923f1c2bf5c4d87d8ae65ed0a4e0613
98913 <98913@LAPTOP-JQR66TAF>
2026-06-23 11:55:05 +0330

    Create Attachment DTOs

---
+++ Application/KanbanBoard.Application/DTOs/Attachment/AttachmentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KanbanBoard.Application.DTOs.Attachment;
+
+public class AttachmentDto
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public long FileSizeInBytes { get; set; }
+    public string ContentType { get; set; } = string.Empty;
+    public Guid TaskId { get; set; }
+    public DateTime UploadedAt { get; set; }
+}
---
+++ Application/KanbanBoard.Application/DTOs/Attachment/CreateAttachmentDto.cs
@@ -0,0 +1,6 @@
+﻿namespace KanbanBoard.Application.DTOs.Attachment;
+
+public class CreateAttachmentDto
+{
+    public IFormFile File { get; set; } = null!;
+}
