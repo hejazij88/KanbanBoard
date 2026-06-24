d64aa452da3aaf458110ebe53e748879e5d90064
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:10:49 +0330

    Add Create Workspace Command

---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/CreateWorkspaceCommand.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class CreateWorkspaceCommand:IRequest<WorkspaceDto>, IRequest<CreateWorkspaceDto>
+{
+    public CreateWorkspaceDto WorkspaceDto { get; set; } = null!;
+}
---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/CreateWorkspaceCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class CreateWorkspaceCommandValidator: AbstractValidator<CreateWorkspaceCommand>
+{
+    public CreateWorkspaceCommandValidator()
+    {
+        RuleFor(a => a.WorkspaceDto.Name).NotEmpty().MaximumLength(100);
+    }
+}
