1c3497eedaf0a014c33efdb59f00e10e75b671d6
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:22:48 +0330

    Add RemoveWorkspaceCommand

---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/RemoveWorkSpaceMemberCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class RemoveWorkSpaceMemberCommandHandler:IRequestHandler<RemoveWorkspaceMemberCommand,bool>
+{
+    public async Task<bool> Handle(RemoveWorkspaceMemberCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
