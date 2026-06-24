41bb45639436909a6c2225a56670cbb3d12ea4ec
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:16:54 +0330

    Add DeleteWorkspaceCommand

---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/DeleteWorkspaceCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class DeleteWorkspaceCommand : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/DeleteWorkspaceCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class DeleteWorkspaceCommandHandler : IRequestHandler<DeleteWorkspaceCommand, bool>
+{
+    public async Task<bool> Handle(DeleteWorkspaceCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
