a0b640c5d8b3fb71310168d106c88f2710ae1007
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:25:36 +0330

    Add GetWorkspaceByIdQuery

---
+++ Application/KanbanBoard.Application/Features/Workspace/Queries/GetWorkspaceByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Queries;
+
+public class GetWorkspaceByIdQuery:IRequest<WorkspaceDto>
+{
+    public Guid WorkSpaceId { get; set; }
+}
---
+++ Application/KanbanBoard.Application/Features/Workspace/Queries/GetWorkspaceByIdQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Queries;
+
+public class GetWorkspaceByIdQueryHandler : IRequestHandler<GetWorkspaceByIdQuery, WorkspaceDto>
+{
+    public async Task<WorkspaceDto> Handle(GetWorkspaceByIdQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
