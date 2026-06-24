ae99e1100928cf5727063615d0f2082f81d7d6b5
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:14:29 +0330

    Add UpdateWorkspaceCommand

---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/UpdateWorkspaceCommand.cs
@@ -0,0 +1,10 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class UpdateWorkspaceCommand : IRequest<WorkspaceDto>
+{
+    public Guid WorkspaceId { get; set; }
+    public UpdateWorkspaceCommand WorkspaceCommand { get; set; }
+}
---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/UpdateWorkspaceCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class UpdateWorkspaceCommandHandler : IRequestHandler<UpdateWorkspaceCommand, WorkspaceDto>
+{
+    public async Task<WorkspaceDto> Handle(UpdateWorkspaceCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
