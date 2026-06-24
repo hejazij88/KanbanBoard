51ebb439ce272ffd8dd39ea0034124f652188596
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:54:25 +0330

    Add GetBoardByIdQueryHandler

---
+++ Application/KanbanBoard.Application/Features/Board/Queries/GetBoardByIdQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Board;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Board.Queries;
+
+public class GetBoardByIdQueryHandler :IRequestHandler<GetBoardByIdQuery,BoardDto>
+{
+    public async Task<BoardDto> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
