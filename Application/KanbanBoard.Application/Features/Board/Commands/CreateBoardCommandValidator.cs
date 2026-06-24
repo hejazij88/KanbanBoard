b8f0b40eb4f478b57419adfd57f20264cb7febe9
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:46:20 +0330

    Add CreateBoardCommand

---
+++ Application/KanbanBoard.Application/Features/Board/Commands/CreateBoardCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace KanbanBoard.Application.Features.Board.Commands;
+
+public class CreateBoardCommandValidator : AbstractValidator<CreateBoardCommand>
+{
+    public CreateBoardCommandValidator()
+    {
+        RuleFor(b => b.BoardDto.Title).NotEmpty().MaximumLength(30);
+    }
+}
