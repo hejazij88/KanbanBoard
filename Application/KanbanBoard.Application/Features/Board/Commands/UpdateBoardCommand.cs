c4d0a14903e12c44156b6d0450c6acb33762f937
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:49:22 +0330

    Add UpdateBoardCommand

---
+++ Application/KanbanBoard.Application/Features/Board/Commands/UpdateBoardCommand.cs
@@ -0,0 +1,10 @@
+﻿using KanbanBoard.Application.DTOs.Board;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Board.Commands;
+
+public class UpdateBoardCommand : IRequest<BoardDto>
+{
+    public Guid  BoardId{ get; set; }
+    public UpdateBoardDto UpdateBoardDto { get; set; }
+}
---
+++ Application/KanbanBoard.Application/Features/Board/Commands/UpdateBoardCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Board;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Board.Commands;
+
+public class UpdateBoardCommandHandler : IRequestHandler<UpdateBoardCommand, BoardDto>
+{
+    public async Task<BoardDto> Handle(UpdateBoardCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
