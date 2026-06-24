ff0671330f00899182afbbedf0da0833877d90b2
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:56:34 +0330

    Add GetBoardsByWorkspaceQuery

---
+++ Application/KanbanBoard.Application/Features/Board/Queries/GetBoardsByWorkspaceQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Board;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Board.Queries;
+
+public class GetBoardsByWorkspaceQueryHandler : IRequestHandler<GetBoardsByWorkspaceQuery, List<BoardDto>>
+{
+    public async Task<List<BoardDto>> Handle(GetBoardsByWorkspaceQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
