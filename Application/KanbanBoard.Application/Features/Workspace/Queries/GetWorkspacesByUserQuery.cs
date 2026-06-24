70fb985026164b77d3afcf2fa0c3eb26a065eb20
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:27:02 +0330

    Add GetWorkspacesByUserQuery

---
+++ Application/KanbanBoard.Application/Features/Workspace/Queries/GetWorkspacesByUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Queries;
+
+public class GetWorkspacesByUserQuery : IRequest<List<WorkspaceDto>>
+{
+    public Guid UserId { get; set; }
+}
