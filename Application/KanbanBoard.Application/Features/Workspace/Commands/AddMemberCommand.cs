b4cf7ce29e1cbb02de2c0e2307188ed9eba276fc
98913 <98913@LAPTOP-JQR66TAF>
2026-06-24 11:20:02 +0330

    Add AddMemberWorkspaceComand

---
+++ Application/KanbanBoard.Application/Features/Workspace/Commands/AddMemberCommand.cs
@@ -0,0 +1,10 @@
+﻿using KanbanBoard.Application.DTOs.Workspace;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Workspace.Commands;
+
+public class AddMemberCommand : IRequest<WorkspaceMemberDto>
+{
+    public Guid WorkspaceId { get; set; }
+    public AddMemberDto AddMemberDto { get; set; } = null!;
+}
