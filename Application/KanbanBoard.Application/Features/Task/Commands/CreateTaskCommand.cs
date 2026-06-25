65c74857b00a502c715c68d66e35dc5a19bd2e68
98913 <98913@LAPTOP-JQR66TAF>
2026-06-25 13:43:26 +0330

    Add CreateTaskCommandHandler

---
+++ Application/KanbanBoard.Application/Features/Task/Commands/CreateTaskCommand.cs
@@ -0,0 +1,11 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Commands;
+
+public class CreateTaskCommand:IRequest<TaskDto>
+{
+    public Guid ColumnId { get; set; }
+    public CreateTaskDto TaskDto { get; set; } = null!;
+
+}
---
+++ Application/KanbanBoard.Application/Features/Task/Commands/CreateTaskCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Commands;
+
+public class CreateTaskCommandHandler:IRequestHandler<CreateTaskCommand,TaskDto>
+{
+    public async Task<TaskDto> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
