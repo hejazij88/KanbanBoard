7dc6330feef21447370e2f98a8fcb0087ea19d7f
98913 <98913@LAPTOP-JQR66TAF>
2026-06-25 13:51:21 +0330

    Add CreateCommentCommand

---
+++ Application/KanbanBoard.Application/Features/Comment/Commands/CreateCommentCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using KanbanBoard.Application.DTOs.Comment;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Comment.Commands;
+
+public class CreateCommentCommandHandler:IRequestHandler<CreateCommentCommand,CommentDto>
+{
+    public async Task<CommentDto> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
