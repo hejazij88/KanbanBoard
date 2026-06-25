eaed4e1f0fecfb18b2943840832b22d78ac49928
98913 <98913@LAPTOP-JQR66TAF>
2026-06-25 13:44:38 +0330

    Add MoveTaskCommandHandler

---
+++ Application/KanbanBoard.Application/Features/Task/Commands/MoveTaskCommand.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Commands;
+
+public class MoveTaskCommand : IRequest<bool>
+{
+    public MoveTaskDto MoveTaskDto { get; set; } = null!;
+}
---
+++ Application/KanbanBoard.Application/Features/Task/Commands/MoveTaskCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Commands;
+
+public class MoveTaskCommandHandler : IRequestHandler<MoveTaskCommand, bool>
+{
+    public async Task<bool> Handle(MoveTaskCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
