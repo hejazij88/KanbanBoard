5ceefd247092bd6b16597a123e30677533d525ac
98913 <98913@LAPTOP-JQR66TAF>
2026-06-25 13:46:52 +0330

    Add GetTaskByIdQueryHandler

---
+++ Application/KanbanBoard.Application/Features/Task/Queries/GetTaskByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Queries;
+
+public class GetTaskByIdQuery:IRequest<TaskDto>
+{
+    public Guid TaskId { get; set; }
+}
