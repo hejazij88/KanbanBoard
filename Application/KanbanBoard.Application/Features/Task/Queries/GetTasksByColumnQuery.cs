6b69907507d02a53b06e0b76d9d40093f9de4a38
98913 <98913@LAPTOP-JQR66TAF>
2026-06-25 13:48:32 +0330

    Add GetTasksByColumnQueryHandler

---
+++ Application/KanbanBoard.Application/Features/Task/Queries/GetTasksByColumnQuery.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Task.Queries;
+
+public class GetTasksByColumnQuery : IRequest<List<TaskDto>>
+{
+    public Guid ColumnId { get; set; }
+}
