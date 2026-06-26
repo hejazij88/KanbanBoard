0a38ced16923f156965c46a1c9608d6c465f61b7
98913 <98913@LAPTOP-JQR66TAF>
2026-06-26 17:43:55 +0330

    Create ITaskRepository

---
+++ Application/KanbanBoard.Application/Interfaces/Repositories/ITaskRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+
+public interface ITaskRepository : IRepository<TaskItem>
+{
+    Task<TaskItem?> GetTaskWithDetailsAsync(Guid taskId);
+
+    Task<IEnumerable<TaskItem>> GetTasksByColumnAsync(Guid columnId);
+
+    Task<IEnumerable<TaskItem>> GetTasksByBoardAsync(Guid boardId);
+
+    Task<IEnumerable<TaskItem>> GetTasksAssignedToUserAsync(Guid userId);
+
+    Task<IEnumerable<TaskItem>> GetTasksByPriorityAsync(Guid boardId, Domain.Enums.Priority priority);
+
+    Task<IEnumerable<TaskItem>> GetTasksByDueDateRangeAsync(Guid boardId, DateTime from, DateTime to);
+
+    Task<int> GetMaxOrderAsync(Guid columnId);
+
+    Task<int> GetTaskCountByColumnAsync(Guid columnId);
+
+    Task<int> GetTaskCountByBoardAsync(Guid boardId);
+
+    Task<IEnumerable<TaskItem>> SearchTasksAsync(Guid boardId, string searchTerm);
+
+    Task ReorderTasksAsync(Guid columnId, int fromOrder, int toOrder);
+}
