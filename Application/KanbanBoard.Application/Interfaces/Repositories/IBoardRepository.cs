28a4bd444eeb076717109fc28039f8944c1930c7
98913 <98913@LAPTOP-JQR66TAF>
2026-06-26 17:41:09 +0330

    Create IBoardRepository

---
+++ Application/KanbanBoard.Application/Interfaces/Repositories/IBoardRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+
+public interface IBoardRepository : IRepository<Board>
+{
+    Task<Board?> GetBoardWithColumnsAndTasksAsync(Guid boardId);
+
+    Task<Board?> GetBoardWithColumnsAsync(Guid boardId);
+    
+    Task<IEnumerable<Board>> GetBoardsByWorkspaceAsync(Guid workspaceId);
+    
+    Task<IEnumerable<Board>> GetBoardsByUserAsync(Guid userId);
+    
+    Task<int> GetBoardCountAsync(Guid workspaceId);
+    
+    Task<bool> IsTitleExistsInWorkspaceAsync(Guid workspaceId, string title, Guid? excludeBoardId = null);
+}
