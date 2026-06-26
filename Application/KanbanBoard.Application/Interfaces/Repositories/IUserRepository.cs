69f9595c877c7711b338145fbe33a93f3eb10728
98913 <98913@LAPTOP-JQR66TAF>
2026-06-26 17:38:05 +0330

    Crete IUserRepository And All Method

---
+++ Application/KanbanBoard.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetUserByEmailAsync(string email);
+
+    Task<User?> GetUserWithDetailsAsync(Guid userId);
+
+    Task<User?> GetUserByRefreshTokenAsync(string refreshToken);
+
+    Task UpdateRefreshTokenAsync(Guid userId, string? refreshToken, DateTime? expiryTime);
+
+    Task<IEnumerable<User>> GetUsersByWorkspaceRoleAsync(Guid workspaceId, string role);
+
+    Task<IEnumerable<User>> GetUsersNotInWorkspaceAsync(Guid workspaceId);
+
+}
