b37b4b8fe06e1fc539a46eb0975d5418f84cc15f
98913 <98913@LAPTOP-JQR66TAF>
2026-06-26 17:42:35 +0330

    Create IColumnRepository

--- Application/KanbanBoard.Application/Interfaces/Repositories/IBoardRepository.cs
+++ Application/KanbanBoard.Application/Interfaces/Repositories/IBoardRepository.cs
@@ -1,16 +1,18 @@
-﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+﻿using System.Data.Common;
+
+namespace KanbanBoard.Application.Interfaces.Repositories;
 
 public interface IBoardRepository : IRepository<Board>
 {
     Task<Board?> GetBoardWithColumnsAndTasksAsync(Guid boardId);
 
     Task<Board?> GetBoardWithColumnsAsync(Guid boardId);
-    
+
     Task<IEnumerable<Board>> GetBoardsByWorkspaceAsync(Guid workspaceId);
-    
+
     Task<IEnumerable<Board>> GetBoardsByUserAsync(Guid userId);
-    
+
     Task<int> GetBoardCountAsync(Guid workspaceId);
-    
+
     Task<bool> IsTitleExistsInWorkspaceAsync(Guid workspaceId, string title, Guid? excludeBoardId = null);
 }
---
+++ Application/KanbanBoard.Application/Interfaces/Repositories/IColumnRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+
+public interface IColumnRepository : IRepository<BoardColumn>
+{
+
+    Task<BoardColumn?> GetColumnWithTasksAsync(Guid columnId);
+
+
+    Task<IEnumerable<BoardColumn>> GetColumnsByBoardWithTasksAsync(Guid boardId);
+
+
+    Task<IEnumerable<BoardColumn>> GetColumnsByBoardAsync(Guid boardId);
+
+
+    Task<int> GetMaxOrderAsync(Guid boardId);
+
+
+    Task ReorderColumnsAsync(Guid boardId, int oldOrder, int newOrder);
+
+    Task<IEnumerable<BoardColumn>> GetDefaultColumnsAsync(Guid boardId);
+}
