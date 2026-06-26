77bd322717f9485eea5fe59ada29f1be31f9fdf5
98913 <98913@LAPTOP-JQR66TAF>
2026-06-26 17:45:11 +0330

    Create ICommentRepository

---
+++ Application/KanbanBoard.Application/Interfaces/Repositories/ICommentRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace KanbanBoard.Application.Interfaces.Repositories;
+
+public interface ICommentRepository : IRepository<Comment>
+{
+    Task<Comment?> GetCommentWithUserAsync(Guid commentId);
+
+    Task<IEnumerable<Comment>> GetCommentsByTaskWithUserAsync(Guid taskId);
+
+    Task<IEnumerable<Comment>> GetCommentsByTaskAsync(Guid taskId);
+
+    Task<IEnumerable<Comment>> GetCommentsByUserAsync(Guid userId);
+
+    Task<int> GetCommentCountByTaskAsync(Guid taskId);
+
+    Task DeleteCommentsByTaskAsync(Guid taskId);
+}
