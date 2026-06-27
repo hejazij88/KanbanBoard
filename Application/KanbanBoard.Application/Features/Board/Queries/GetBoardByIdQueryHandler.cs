b9229330e8ec248a9f34a38488102b85bb83ba74
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 14:21:16 +0330

    Impeliment GetBoardByIdQueryHandler

--- Application/KanbanBoard.Application/Features/Board/Queries/GetBoardByIdQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Board/Queries/GetBoardByIdQueryHandler.cs
@@ -1,12 +1,27 @@
-﻿using KanbanBoard.Application.DTOs.Board;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Board;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Board.Queries;
 
 public class GetBoardByIdQueryHandler :IRequestHandler<GetBoardByIdQuery,BoardDto>
 {
+    private readonly IBoardRepository _boardRepo;
+    private readonly IMapper _mapper;
+
+    public GetBoardByIdQueryHandler(IBoardRepository boardRepo, IMapper mapper)
+    {
+        _boardRepo = boardRepo;
+        _mapper = mapper;
+    }
+
     public async Task<BoardDto> Handle(GetBoardByIdQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var board = await _boardRepo.GetBoardWithColumnsAndTasksAsync(request.BoardId);
+        if (board == null)
+            throw new Exception("Board not found.");
+
+        return _mapper.Map<BoardDto>(board);
     }
 }
