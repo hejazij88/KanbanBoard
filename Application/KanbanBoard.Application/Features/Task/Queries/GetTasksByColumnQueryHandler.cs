4101189b6bea397b99332ca5573a991300e9bdd4
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 17:16:51 +0330

    Impeliment GetTasksByColumnQueryHandler

--- Application/KanbanBoard.Application/Features/Task/Queries/GetTasksByColumnQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Task/Queries/GetTasksByColumnQueryHandler.cs
@@ -1,12 +1,26 @@
-﻿using KanbanBoard.Application.DTOs.Task;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Task;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Task.Queries;
 
-public class GetTasksByColumnQueryHandler:IRequestHandler<GetTasksByColumnQuery,List<TaskDto>>
+public class GetTasksByColumnQueryHandler : IRequestHandler<GetTasksByColumnQuery, List<TaskDto>>
 {
+
+    private readonly ITaskRepository _taskRepository;
+    private readonly IMapper _mapper;
+
+    public GetTasksByColumnQueryHandler(IMapper mapper, ITaskRepository taskRepository)
+    {
+        _mapper = mapper;
+        _taskRepository = taskRepository;
+    }
+
     public async Task<List<TaskDto>> Handle(GetTasksByColumnQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var listTask = await _taskRepository.GetTasksByColumnAsync(request.ColumnId);
+
+        return _mapper.Map<List<TaskDto>>(listTask);
     }
 }
