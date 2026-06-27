c2243f2228e10091620245f938f1bf489febbd3d
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 15:04:10 +0330

    Impeliment GetColumnsByBoardQueryHandler

--- Application/KanbanBoard.Application/Features/Column/Queries/GetColumnsByBoardQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Column/Queries/GetColumnsByBoardQueryHandler.cs
@@ -1,12 +1,24 @@
-﻿using KanbanBoard.Application.DTOs.Column;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Column;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Column.Queries;
 
 public class GetColumnsByBoardQueryHandler:IRequestHandler<GetColumnsByBoardQuery,List<ColumnDto>>
 {
+    private readonly IColumnRepository _columnRepo;
+    private readonly IMapper _mapper;
+
+    public GetColumnsByBoardQueryHandler(IColumnRepository columnRepo, IMapper mapper)
+    {
+        _columnRepo = columnRepo;
+        _mapper = mapper;
+    }
+
     public async Task<List<ColumnDto>> Handle(GetColumnsByBoardQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var columns = await _columnRepo.GetColumnsByBoardWithTasksAsync(request.BoardId);
+        return _mapper.Map<List<ColumnDto>>(columns.OrderBy(c => c.Order));
     }
 }
