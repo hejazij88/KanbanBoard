f3a691fc1974308952306f7dc040ce647a927ac6
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 13:56:07 +0330

    Impeliment GetWorkspaceByIdQueryHandler

--- Application/KanbanBoard.Application/Features/Workspace/Queries/GetWorkspaceByIdQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Workspace/Queries/GetWorkspaceByIdQueryHandler.cs
@@ -1,12 +1,27 @@
-﻿using KanbanBoard.Application.DTOs.Workspace;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Workspace;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Workspace.Queries;
 
 public class GetWorkspaceByIdQueryHandler : IRequestHandler<GetWorkspaceByIdQuery, WorkspaceDto>
 {
+    private readonly IWorkspaceRepository _workspaceRepo;
+    private readonly IMapper _mapper;
+
+    public GetWorkspaceByIdQueryHandler(IWorkspaceRepository workspaceRepo, IMapper mapper)
+    {
+        _workspaceRepo = workspaceRepo;
+        _mapper = mapper;
+    }
+
     public async Task<WorkspaceDto> Handle(GetWorkspaceByIdQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var workspace = await _workspaceRepo.GetByIdAsync(request.WorkSpaceId); 
+        if (workspace == null)
+            throw new Exception("Workspace not found.");
+
+        return _mapper.Map<WorkspaceDto>(workspace);
     }
 }
