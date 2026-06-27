b87f662247d1f29f29ac2beda426d384c8690ac9
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 13:16:26 +0330

    Create IJwtService

---
+++ Application/KanbanBoard.Application/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace KanbanBoard.Application.Interfaces.Services;
+
+public interface IJwtService
+{
+    string GenerateToken(User user);
+    
+    string GenerateRefreshToken();
+    
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+   
+    bool ValidateToken(string token);
+}
