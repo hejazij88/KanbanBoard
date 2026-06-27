ba251f28ba1459e6ade75640c2cce46ece84cf31
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 17:28:16 +0330

    Impeliment GetCommentsByTaskQueryHandler

--- Application/KanbanBoard.Application/Features/Comment/Queries/GetCommentsByTaskQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Comment/Queries/GetCommentsByTaskQueryHandler.cs
@@ -1,12 +1,23 @@
-﻿using KanbanBoard.Application.DTOs.Comment;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Comment;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Comment.Queries;
 
 public class GetCommentsByTaskQueryHandler : IRequestHandler<GetCommentsByTaskQuery, List<CommentDto>>
 {
+    private readonly ICommentRepository _commentRepository;
+    private readonly IMapper _mapper;
+    public GetCommentsByTaskQueryHandler(ICommentRepository commentRepository, IMapper mapper)
+    {
+        _commentRepository = commentRepository;
+        _mapper = mapper;
+    }
+
     public async Task<List<CommentDto>> Handle(GetCommentsByTaskQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var list = await _commentRepository.GetCommentsByTaskAsync(request.TaskId);
+        return _mapper.Map<List<CommentDto>>(list);
     }
 }
