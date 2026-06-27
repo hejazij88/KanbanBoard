c344ac49a1ac49ef5c1e8ac02c63f16648f0d4b9
98913 <98913@LAPTOP-JQR66TAF>
2026-06-27 14:21:57 +0330

    Impeliment GetBoardsByWorkspaceQueryHandler

--- Application/KanbanBoard.Application/Features/Board/Queries/GetBoardsByWorkspaceQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Board/Queries/GetBoardsByWorkspaceQueryHandler.cs
@@ -1,12 +1,24 @@
-﻿using KanbanBoard.Application.DTOs.Board;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Board;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Board.Queries;
 
 public class GetBoardsByWorkspaceQueryHandler : IRequestHandler<GetBoardsByWorkspaceQuery, List<BoardDto>>
 {
+    private readonly IBoardRepository _boardRepo;
+    private readonly IMapper _mapper;
+
+    public GetBoardsByWorkspaceQueryHandler(IBoardRepository boardRepo, IMapper mapper)
+    {
+        _boardRepo = boardRepo;
+        _mapper = mapper;
+    }
+
     public async Task<List<BoardDto>> Handle(GetBoardsByWorkspaceQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var boards = await _boardRepo.GetBoardsByWorkspaceAsync(request.WorkspaceId);
+        return _mapper.Map<List<BoardDto>>(boards);
     }
 }
