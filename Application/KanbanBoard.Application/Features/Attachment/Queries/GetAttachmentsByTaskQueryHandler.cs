3ab2204860f5c569cceeb2c3e7413c2c51b8f3e7
98913 <98913@LAPTOP-JQR66TAF>
2026-06-28 13:07:15 +0330

    Impeliment GetAttachmentsByTaskQueryHandler

--- Application/KanbanBoard.Application/Features/Attachment/Queries/GetAttachmentsByTaskQueryHandler.cs
+++ Application/KanbanBoard.Application/Features/Attachment/Queries/GetAttachmentsByTaskQueryHandler.cs
@@ -1,12 +1,24 @@
-﻿using KanbanBoard.Application.DTOs.Attachment;
+﻿using AutoMapper;
+using KanbanBoard.Application.DTOs.Attachment;
+using KanbanBoard.Application.Interfaces.Repositories;
 using MediatR;
 
 namespace KanbanBoard.Application.Features.Attachment.Queries;
 
 public class GetAttachmentsByTaskQueryHandler:IRequestHandler<GetAttachmentsByTaskQuery,List<AttachmentDto>>
 {
+    private readonly IAttachmentRepository _attachmentRepo;
+    private readonly IMapper _mapper;
+
+    public GetAttachmentsByTaskQueryHandler(IAttachmentRepository attachmentRepo, IMapper mapper)
+    {
+        _attachmentRepo = attachmentRepo;
+        _mapper = mapper;
+    }
+
     public async Task<List<AttachmentDto>> Handle(GetAttachmentsByTaskQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var attachments = await _attachmentRepo.GetAttachmentsByTaskAsync(request.TaskId);
+        return _mapper.Map<List<AttachmentDto>>(attachments);
     }
 }
