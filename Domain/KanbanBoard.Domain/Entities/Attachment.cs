8e2953a83f802569beb04f88a59ae7c5e2b72d18
98913 <98913@LAPTOP-JQR66TAF>
2026-06-29 14:20:30 +0330

    Update Attachment Entity

--- Domain/KanbanBoard.Domain/Entities/Attachment.cs
+++ Domain/KanbanBoard.Domain/Entities/Attachment.cs
@@ -1,12 +1,31 @@
-﻿    namespace KanbanBoard.Domain.Entities;
+﻿    using KanbanBoard.Domain.Common;
 
-public class Attachment
+    namespace KanbanBoard.Domain.Entities;
+
+public class Attachment:AuditableEntity
 {
-    public Guid Id { get; set; }
-    public string FileName { get; set; } = string.Empty;
-    public string FilePath { get; set; } = string.Empty;
-    public long FileSize { get; set; }
-    public Guid TaskId { get; set; }
-    public TaskItem Task { get; set; } = null!;
-    public DateTime UploadedAt { get; set; }
+    public string FileName { get;private set; } = string.Empty;
+    public string FilePath { get;private set; } = string.Empty;
+    public long FileSize { get; private set; }
+    public string ContentType { get; private set; }
+    public Guid TaskId { get; private set; }
+    public TaskItem Task { get; private set; } = null!;
+    public DateTime UploadedAt { get;private set; }
+
+
+    public Attachment()
+    {
+        
+    }
+
+    public Attachment(string fileName, string filePath, long fileSize, string contentType, Guid taskId)
+    {
+        Id = Guid.NewGuid();
+        FileName = fileName;
+        FilePath = filePath;
+        fileSize = fileSize;
+        ContentType = contentType;
+        TaskId = taskId;
+        UploadedAt = DateTime.UtcNow;
+    }
 }
