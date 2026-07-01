fa2b8509c3c5a24570ca4e0fbecd37a23d652458
98913 <98913@LAPTOP-JQR66TAF>
2026-07-01 14:12:44 +0330

    Create GetColumnByIdQuery class

---
+++ Application/KanbanBoard.Application/Features/Column/Queries/GetColumnByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Application.DTOs.Column;
+using MediatR;
+
+namespace KanbanBoard.Application.Features.Column.Queries;
+
+public class GetColumnByIdQuery:IRequest<ColumnDto>
+{
+    public Guid Id { get; set; }
+}
