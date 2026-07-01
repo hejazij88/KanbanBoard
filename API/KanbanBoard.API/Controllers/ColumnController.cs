b688bdbbd6063d1c9ca8f48f276ed527a7a5fe11
98913 <98913@LAPTOP-JQR66TAF>
2026-07-01 14:09:10 +0330

    Add Method GetColumnsByBoard

--- API/KanbanBoard.API/Controllers/ColumnController.cs
+++ API/KanbanBoard.API/Controllers/ColumnController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KanbanBoard.Application.Features.Column.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KanbanBoard.API.Controllers
 {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class ColumnController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public ColumnController(IMediator mediator)
         {
-             
+            _mediator = mediator;
+        }
+
+        [HttpGet("board/{boardId}")]
+        public async Task<IActionResult> GetColumnsByBoard(Guid boardId)
+        {
+            var query = new GetColumnsByBoardQuery { BoardId = boardId };
+            var result = await _mediator.Send(query);
+            return Ok(result);
         }
     }
 }
