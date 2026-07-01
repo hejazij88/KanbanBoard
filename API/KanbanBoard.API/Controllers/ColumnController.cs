64b5b6361ec3222562f757ff4005aaaf1f8f56e8
98913 <98913@LAPTOP-JQR66TAF>
2026-07-01 14:11:22 +0330

    Add CreateColumn Method

--- API/KanbanBoard.API/Controllers/ColumnController.cs
+++ API/KanbanBoard.API/Controllers/ColumnController.cs
@@ -1,4 +1,6 @@
-﻿using KanbanBoard.Application.Features.Column.Queries;
+﻿using KanbanBoard.Application.Features.Column.Commands;
+using KanbanBoard.Application.Features.Column.Queries;
+using KanbanBoard.Application.Features.Task.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,20 @@ namespace KanbanBoard.API.Controllers
             var query = new GetColumnsByBoardQuery { BoardId = boardId };
             var result = await _mediator.Send(query);
             return Ok(result);
+        }
+        [HttpPost("board/{boardId}")]
+        public async Task<IActionResult> CreateColumn(Guid boardId, [FromBody] CreateColumnCommand command)
+        {
+            command.BoardId = boardId;
+            try
+            {
+                var result = await _mediator.Send(command);
+                //return CreatedAtAction(nameof(GetColumnById), new { id = result.Id }, result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
     }
 }
