463a9d35bad5e599781e61af5f1dedfdc5c7f765
98913 <98913@LAPTOP-JQR66TAF>
2026-07-01 14:39:07 +0330

    Add GetCommentsByTask Method

--- API/KanbanBoard.API/Controllers/CommentController.cs
+++ API/KanbanBoard.API/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using KanbanBoard.Application.Features.Comment.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,5 +11,19 @@ namespace KanbanBoard.API.Controllers
     [Authorize]
     public class CommentController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CommentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("task/{taskId}")]
+        public async Task<IActionResult> GetCommentsByTask(Guid taskId)
+        {
+            var query = new GetCommentsByTaskQuery { TaskId = taskId };
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
     }
 }
