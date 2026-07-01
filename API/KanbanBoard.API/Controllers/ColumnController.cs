62321d3696e8f409405c59287c548a3ce60bfbb4
98913 <98913@LAPTOP-JQR66TAF>
2026-07-01 14:08:27 +0330

    Create ColumnController

---
+++ API/KanbanBoard.API/Controllers/ColumnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanBoard.API.Controllers
+{
+    public class ColumnController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+             
+        }
+    }
+}
