cb5269441ab5f2a83301258d127429f8aca24ec6
98913 <98913@LAPTOP-JQR66TAF>
2026-07-07 13:01:14 +0330

    Add ColumnCard Dialog

---
+++ Presentation/KanbanBoard.Presentation/Components/Pages/Column/Components/ColumnCard.razor.cs
@@ -0,0 +1,20 @@
+﻿using KanbanBoard.Application.DTOs.Column;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace KanbanBoard.Presentation.Components.Pages.Column.Components;
+
+public partial class ColumnCard
+{
+    [Parameter] public ColumnDto Column { get; set; } = null!;
+    [Parameter] public EventCallback OnTaskMoved { get; set; }
+
+    private async Task OpenCreateTaskDialog()
+    {
+        var parameters = new DialogParameters { { "ColumnId", Column.Id } };
+        var dialog = await DialogService.ShowAsync<CreateTaskDialog>("Add Task", parameters);
+        var result = await dialog.Result;
+        if (!result.Canceled)
+            await OnTaskMoved.InvokeAsync();
+    }
+}
