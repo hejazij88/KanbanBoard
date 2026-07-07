3ed907038fdad2f1a6b9457f1e6c8776afb2c798
98913 <98913@LAPTOP-JQR66TAF>
2026-07-07 13:02:45 +0330

    Add CreateColumnDialog

---
+++ Presentation/KanbanBoard.Presentation/Components/Pages/Column/Components/CreateColumnDialog.razor.cs
@@ -0,0 +1,33 @@
+﻿using KanbanBoard.Application.DTOs.Column;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace KanbanBoard.Presentation.Components.Pages.Column.Components;
+
+public partial class CreateColumnDialog
+{
+    [CascadingParameter] private DialogParameters? Parameters { get; set; }
+    private CreateColumnDto _model = new();
+
+    protected override void OnInitialized()
+    {
+        if (Parameters != null && Parameters.TryGetValue("BoardId", out object? boardId))
+        {
+            // BoardId در خود Command استفاده می‌شود و در DTO نیست
+        }
+    }
+
+    private async Task CreateColumn()
+    {
+        try
+        {
+            var boardId = Parameters!.Get<Guid>("BoardId");
+            var result = await ColumnApiService.CreateColumnAsync(boardId, _model);
+            Dialog.CloseAsync(DialogResult.Ok(result));
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
+}
