8f178e6fe35ff063d4e261d1f3d70e7dd4d65037
98913 <98913@LAPTOP-JQR66TAF>
2026-07-07 13:05:28 +0330

    Add CreateTaskDialog

---
+++ Presentation/KanbanBoard.Presentation/Components/Pages/Task/Components/CreateTaskDialog.razor.cs
@@ -0,0 +1,32 @@
+﻿using KanbanBoard.Application.DTOs.Task;
+using KanbanBoard.Domain.Enums;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace KanbanBoard.Presentation.Components.Pages.Task.Components;
+
+public partial class CreateTaskDialog
+{
+    [CascadingParameter] private DialogParameters? Parameters { get; set; }
+    private CreateTaskDto _model = new() { Priority = Priority.Medium };
+    private Guid _columnId;
+
+    protected override void OnInitialized()
+    {
+        if (Parameters != null && Parameters.TryGetValue("ColumnId", out object? columnId))
+            _columnId = (Guid)columnId!;
+    }
+
+    private async System.Threading.Tasks.Task CreateTask()
+    {
+        try
+        {
+            var result = await TaskApiService.CreateTaskAsync(_columnId, _model);
+            Dialog.CloseAsync(DialogResult.Ok(result));
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
+}
