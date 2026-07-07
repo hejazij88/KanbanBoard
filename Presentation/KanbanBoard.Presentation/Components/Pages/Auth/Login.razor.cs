b260fdad3f52b7944c56cd30848729db3655f93b
98913 <98913@LAPTOP-JQR66TAF>
2026-07-07 11:18:11 +0330

    Add Page login

---
+++ Presentation/KanbanBoard.Presentation/Components/Pages/Auth/Login.razor.cs
@@ -0,0 +1,29 @@
+﻿using KanbanBoard.Application.DTOs.Auth;
+using KanbanBoard.Presentation.Services;
+using MudBlazor;
+
+namespace KanbanBoard.Presentation.Components.Pages.Auth;
+
+public partial class Login
+{
+    private LogInDto _loginModel = new();
+
+    private async Task HandleLogin()
+    {
+        try
+        {
+            var response = await AuthApiService.LoginAsync(_loginModel);
+            if (response != null)
+            {
+                var authProvider = (CustomAuthenticationStateProvider)AuthProvider;
+                await authProvider.NotifyUserAuthentication(response.Token);
+                Snackbar.Add("Login successful!", Severity.Success);
+                Navigation.NavigateTo("/workspaces");
+            }
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
+}
